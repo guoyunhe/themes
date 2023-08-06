a4879f936269ec29186b9a43c4eac5a553cac991
Guo Yunhe <i@guoyunhe.me>
2023-08-06 12:44:50 +0800

    font tables

---
+++ database/migrations/1688216736805_fonts.ts
@@ -0,0 +1,35 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'fonts';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id');
+
+      table.integer('user_id').unsigned().nullable().references('users.id').onDelete('SET NULL');
+
+      table.string('family').notNullable().index();
+      table.string('family_zh').nullable().index();
+      table.string('designer').nullable().index();
+      table.string('designer_url').nullable();
+      table.string('manufacturer').nullable().index();
+      table.string('manufacturer_url').nullable();
+      table.string('license').nullable();
+      table.string('license_url').nullable();
+      table.string('version').nullable();
+
+      table.string('source_type').notNullable().index().defaultTo('upload');
+      table.json('source_config').notNullable();
+
+      table.integer('downloads').unsigned().notNullable().index().defaultTo(0);
+
+      table.timestamp('created_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+      table.timestamp('updated_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
---
+++ database/migrations/1688216743512_font_files.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'font_files';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id');
+
+      table.integer('font_id').unsigned().nullable().references('fonts.id').onDelete('CASCADE');
+
+      table.string('sub_family').notNullable().index();
+      table.string('filename').notNullable().index();
+      table.integer('size').unsigned().notNullable();
+      table.string('md5').notNullable();
+      table.json('fvar').nullable();
+
+      table.timestamp('created_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+      table.timestamp('updated_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
