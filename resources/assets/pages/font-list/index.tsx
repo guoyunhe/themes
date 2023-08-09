d8e5b9987fe12b1c197def68079044ca4e3de605
Guo Yunhe <i@guoyunhe.me>
2023-08-09 20:48:51 +0800

    font pagination

--- resources/assets/pages/font-list/index.tsx
+++ resources/assets/pages/font-list/index.tsx
@@ -1,29 +1,42 @@
-import { Box, Container, List } from '@mui/material';
-import { useState } from 'react';
+import { Box, Card, CardHeader, Container, Pagination } from '@mui/material';
 import { useFetch } from 'react-fast-fetch';
+import { useSearchParams } from 'react-router-dom';
 import Paginated from '../../types/Paginated';
 import Font from '../../types/models/Font';
 
 export default function FontListPage() {
-  const [page, setPage] = useState(1);
-  const { data } = useFetch<Paginated<Font>>(`/fonts?page=${page}&perPage=20`);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { data } = useFetch<Paginated<Font>>(`/fonts?${searchParams.toString()}`);
+  const page = Number(searchParams.get('page') || 1);
+  const totalPage = data?.meta ? Math.ceil(data.meta.total / data.meta.perPage) : 1;
 
   return (
     <Box sx={{ overflow: 'auto' }}>
       <Container>
-        <List>
+        <Box sx={{ my: 3 }}>
           {data?.data?.map((item) => (
-            <div key={item.id}>
-              <div>{item.family}</div>
-
+            <Card key={item.id} sx={{ mb: 3 }}>
+              <CardHeader title={item.family} />
               <svg
                 viewBox="0 0 500 25"
                 fill="currentColor"
                 dangerouslySetInnerHTML={{ __html: item.files?.[0]?.langs?.[0]?.preview }}
               ></svg>
-            </div>
+            </Card>
           ))}
-        </List>
+        </Box>
+        <Pagination
+          count={totalPage}
+          page={page}
+          onChange={(_e, p) =>
+            setSearchParams((prev) => {
+              const next = new URLSearchParams(prev);
+              next.set('page', String(p));
+              return next;
+            })
+          }
+          sx={{ my: 3 }}
+        />
       </Container>
     </Box>
   );
